ec66b98a191250b5d0d020a7453e7d647b37dd97
martov656 <martintovarek2@gmail.com>
2025-08-29 07:43:14 +0200

    Push

--- tests/test-6.spec.ts
+++ tests/test-6.spec.ts
@@ -41,3 +41,24 @@ test('testnicole', async ({ page }) => {
   await expect(page.locator('h1')).toContainText('Svatba roku');
 
 });
+
+test('testnicole5', async ({ page }) => {
+   await page.setViewportSize({ width: 1920, height: 1080 });
+  await page.goto('https://www.csfd.cz/');
+  await page.getByRole('button', { name: 'Souhlasit a zavřít: Souhlasit' }).click();
+  await page.getByRole('combobox', { name: 'Vyhledávání' }).click();
+  await page.getByRole('combobox', { name: 'Vyhledávání' }).fill('Nicole Kidman');
+  await page.getByRole('button').filter({ hasText: 'Hledat' }).click();
+  await page.getByRole('heading', { name: 'Nicole Kidman', exact: true }).getByRole('link').click();
+
+
+  
+  await expect(page.locator('h1')).toContainText('Nicole Kidman');
+
+
+  await page.getByRole('row', { name: 'Sedmilhářky' }).getByRole('link').click();
+
+
+  await expect(page.locator('h1')).toContainText('Sedmilhářky');
+
+});
