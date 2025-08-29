3d7ed8bff61a9a5c56fbcb00016d2027cbea7f48
martov656 <martintovarek2@gmail.com>
2025-08-29 07:36:37 +0200

    Push notebook

--- tests/test-6.spec.ts
+++ tests/test-6.spec.ts
@@ -20,3 +20,24 @@ test('test', async ({ page }) => {
   await expect(page.locator('h1')).toContainText('The Morning Show');
 
 });
+
+test('testnicole', async ({ page }) => {
+   await page.setViewportSize({ width: 1920, height: 1080 });
+  await page.goto('https://www.csfd.cz/');
+  await page.getByRole('button', { name: 'Souhlasit a zavřít: Souhlasit' }).click();
+  await page.getByRole('combobox', { name: 'Vyhledávání' }).click();
+  await page.getByRole('combobox', { name: 'Vyhledávání' }).fill('Nicole Kidman');
+  await page.getByRole('button').filter({ hasText: 'Hledat' }).click();
+  await page.getByRole('heading', { name: 'Nicole Kidman', exact: true }).getByRole('link').click();
+
+
+  
+  await expect(page.locator('h1')).toContainText('Nicole Kidman');
+
+
+  await page.getByRole('row', { name: 'Svatba roku' }).getByRole('link').click();
+
+
+  await expect(page.locator('h1')).toContainText('Svatba roku');
+
+});
