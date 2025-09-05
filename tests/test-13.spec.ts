9d3f7a16975ee5d531d225ff551ff9b5c8dac3cd
martov656 <martintovarek2@gmail.com>
2025-09-05 08:51:12 +0200

    pc

--- tests/test-13.spec.ts
+++ tests/test-13.spec.ts
@@ -1,41 +1,45 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://www.super.cz/');
   
-  
-  
-  await page.getByRole('textbox', { name: 'Vyhledat' }).fill('Victoria Silvstedt');
-  await page.getByRole('button', { name: 'Vyhledat' }).click();
 
+await page.goto('https://en.wikipedia.org/wiki/Main_Page');
+await page.getByRole('searchbox', { name: 'Search Wikipedia' }).click();
+await page.getByRole('combobox', { name: 'Search Wikipedia' }).fill('Victoria Silvstedt');
+await page.getByRole('link', { name: 'Victoria Silvstedt Swedish' }).click();
+await expect(page.locator('#firstHeading')).toContainText('Victoria Silvstedt');
+await page.getByRole('link', { name: 'Ulrika Ericsson' }).click();
+await expect(page.locator('#mw-content-text')).toContainText('Ulrika Ericsson');
+page.goBack()
 
-  await expect(page.locator('#top-row-content-wrapper-ref-element')).toContainText('Victoria Silvstedt');
+await page.getByRole('cell', { name: 'Silvstedt at the 2019 Cannes' }).locator('span').getByRole('link').click();
 
-  await page.getByRole('link', { name: 'Victoria Silvstedt – Wikipedie' }).click();
-  await expect(page.locator('#firstHeading')).toContainText('Victoria Silvstedt');
-  
   
 
 });
 
-test('testreese2', async ({ page }) => {
-  await page.goto('https://www.super.cz/');
+
+
+test('testReese', async ({ page }) => {
+  
+await page.goto('https://en.wikipedia.org/wiki/Main_Page');
+
+await page.getByRole('searchbox', { name: 'Search Wikipedia' }).click();
+await page.getByRole('combobox', { name: 'Search Wikipedia' }).fill('Reese Witherspoon');
+await page.getByRole('link', { name: 'Reese Witherspoon American' }).click();
+await expect(page.locator('#firstHeading')).toContainText('Reese Witherspoon');
+
+
+await page.getByRole('link', { name: 'Reese Witherspoon filmography' }).click();
+await expect(page.locator('#firstHeading')).toContainText('Reese Witherspoon filmography');
+
+await page.getByRole('cell', { name: 'This Means War' }).click();
+await expect(page.locator('#firstHeading')).toContainText('This Means War');
+await page.getByRole('link', { name: 'Two men in suits, a woman' }).click();
+
   
 
-await page.getByRole('textbox', { name: 'Vyhledat' }).fill('Reese Witherspoon');
 
-await page.getByRole('button', { name: 'Reese Witherspoonová', exact: true }).click();
-
- 
-await page.getByRole('link', { name: 'Reese Witherspoonová –' }).click();
-await expect(page.locator('#firstHeading')).toContainText('Reese Witherspoonová');
-await page.getByRole('link', { name: 'Pravá blondýnka' }).first().click();
-await expect(page.locator('#firstHeading')).toContainText('Pravá blondýnka');
-
-
-  
-
-  
 
 });
 
@@ -156,29 +160,7 @@ await expect(page.locator('h1')).toContainText('Reese Witherspoon');
 await page.getByRole('link', { name: 'Který je ten pravý?' }).first().click();
 
 
-  
 
-});
-
-
-
-
-
-test('testvic15', async ({ page }) => {
-  await page.goto('https://www.super.cz/');
-  
-  
-  
-  await page.getByRole('textbox', { name: 'Vyhledat' }).fill('Victoria Silvstedt');
-  await page.getByRole('button', { name: 'Vyhledat' }).click();
-
-
-  await expect(page.locator('#top-row-content-wrapper-ref-element')).toContainText('Victoria Silvstedt');
-
-  await page.getByRole('link', { name: 'Victoria Silvstedt | ČSFD.cz' }).click();
-await page.getByRole('button', { name: 'Souhlasit a zavřít: Souhlasit' }).click();
-await expect(page.locator('h1')).toContainText('Victoria Silvstedt');
-await page.getByRole('link', { name: 'Plnou parou vzad' }).first().click();
 
   });
 
--- tests/test-18.spec.ts
+++ tests/test-18.spec.ts
@@ -23,3 +23,95 @@ await expect(page1.locator('h1')).toContainText('Srdečně vás zveme');
 
 
 });
+
+
+test('testkate', async ({ page }) => {
+  
+await page.goto('https://en.wikipedia.org/wiki/Main_Page');
+
+await page.getByRole('searchbox', { name: 'Search Wikipedia' }).click();
+await page.getByRole('combobox', { name: 'Search Wikipedia' }).fill('Kate Winslet');
+await page.getByRole('link', { name: 'Kate Winslet English' }).click();
+await expect(page.locator('#firstHeading')).toContainText('Kate Winslet');
+
+
+await page.getByRole('link', { name: 'Titanic', exact: true }).nth(1).click();
+await expect(page.locator('#firstHeading')).toContainText('Titanic');
+await page.locator('div:nth-child(2) > .thumbimage > span > .mw-file-description').click();
+await expect(page.locator('.mw-mmv-final-image')).toBeVisible();
+
+
+  
+
+});
+
+
+test('testkateb', async ({ page }) => {
+  
+await page.goto('https://en.wikipedia.org/wiki/Main_Page');
+
+await page.getByRole('searchbox', { name: 'Search Wikipedia' }).click();
+await page.getByRole('combobox', { name: 'Search Wikipedia' }).fill('Kate Beckinsale');
+await page.getByRole('link', { name: 'Kate Beckinsale English' }).click();
+
+
+await page.getByRole('row', { name: '2001 Pearl Harbor Nurse Lt.' }).getByRole('link').click();
+await expect(page.locator('#firstHeading')).toContainText('Pearl Harbor');
+await page.getByRole('listitem').filter({ hasText: /^Kate Beckinsale$/ }).getByRole('link').click();
+await expect(page.locator('#firstHeading')).toContainText('Kate Beckinsale');
+await page.getByRole('figure', { name: 'Beckinsale at the 2008' }).getByRole('link').first().click();
+await expect(page.getByRole('img', { name: 'undefined' })).toBeVisible();
+
+
+});
+
+test('testjou', async ({ page }) => {
+  
+await page.goto('https://en.wikipedia.org/wiki/Main_Page');
+
+
+
+await page.getByRole('searchbox', { name: 'Search Wikipedia' }).click();
+await page.getByRole('combobox', { name: 'Search Wikipedia' }).fill('Journey ');
+await page.getByRole('link', { name: 'Journey (band) American rock' }).click();
+await expect(page.locator('#firstHeading')).toContainText('Journey (band)');
+await expect(page.locator('#toc-Discography').getByRole('link', { name: 'Discography' })).toBeVisible();
+await page.getByRole('link', { name: 'Studio albums' }).click();
+await page.getByRole('listitem').filter({ hasText: 'Frontiers (1983)' }).getByRole('link').click();
+await expect(page.locator('#firstHeading')).toContainText('Frontiers');
+await page.getByRole('listitem').filter({ hasText: 'Steve Perry – lead vocals' }).getByRole('link').click();
+await expect(page.locator('#firstHeading')).toContainText('Steve Perry');
+
+
+
+
+});
+
+
+test('testkatebeva', async ({ page }) => {
+  
+await page.goto('https://en.wikipedia.org/wiki/Main_Page');
+
+await page.getByRole('searchbox', { name: 'Search Wikipedia' }).click();
+await page.getByRole('combobox', { name: 'Search Wikipedia' }).fill('Eva Longoria');
+await page.getByRole('link', { name: 'Eva Longoria American' }).click();
+
+
+await page.getByRole('link', { name: 'Eva Longoria filmography' }).click();
+await expect(page.locator('#firstHeading')).toContainText('Eva Longoria filmography');
+await page.getByRole('link', { name: 'The Sentinel' }).click();
+
+await expect(page.locator('#firstHeading')).toContainText('The Sentinel');
+await page.getByRole('listitem').filter({ hasText: /^Eva Longoria$/ }).click();
+await page.getByRole('listitem').filter({ hasText: /^Eva Longoria$/ }).getByRole('link').click();
+await expect(page.locator('#firstHeading')).toContainText('Eva Longoria');
+await page.getByRole('figure', { name: 'Longoria at the 2006 Cannes' }).getByRole('link').click();
+await expect(page.getByRole('img', { name: 'undefined' })).toBeVisible();
+
+
+
+
+
+
+});
+
